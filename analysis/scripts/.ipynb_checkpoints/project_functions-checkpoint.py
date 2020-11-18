4420a9e887a54e76ed58e334cf604297ce053961
minhhbt <buithienminh@gmail.com>
2020-11-18 14:08:48 -0500

    Removed extra member folder, updated load_and_process method.

--- analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
+++ analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -1,10 +1,15 @@
 import pandas as pd
-import numpy as np
 
 def load_and_process(url_or_path_to_csv_file):
 
-        # we are supposed to add the method chain here      
-    df1 = pd.read_csv(url_or_path_to_csv_file).dropna().assign
+    # We are supposed to add the method chain here      
+    df1 = (
+        pd.read_csv(url)
+        .dropna()
+        .assign(GoalReachedPercentage = lambda x: x["pledged"] / x["goal"] * 100)
+        .sort_values("GoalReachedPercentage", ascending = False)
+        .reset_index(drop=True)
+    )
     return df1
           
       
